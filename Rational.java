dd4364b941f52b67b5a7da63192b2d611c048ba8
Tircnais <tircnais.ca@gmail.com>
2018-10-14 21:49:05 -0500

    uso de stream (secuencia/flujo) para no usar la interaccion

--- Rational.java
+++ Rational.java
@@ -1,3 +1,8 @@
+package taller1;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Rational {
     public static Rational ZERO = new Rational(0, 1);
 
@@ -75,6 +80,48 @@ public class Rational {
     public String toString() {
         var output = denominator == 1 ? String.valueOf(numerator) : String.format("%d/%d", numerator, denominator);
         return output;
+    }
+    
+    public static void main(String[] args) {
+        var rationals = List.of(
+            new Rational(1, 4),
+            new Rational(1, 8),
+            new Rational(3, 4),
+            new Rational(7, 8)
+        );
+        
+        //Imperativo
+        var unMedio = new Rational(1, 2);
+        for (Rational object : rationals) {
+            if(object.isLessThan(unMedio))
+                System.out.println(object);
+        }
+        
+        //FUNCIONAL
+        //Obtener todos los números racionales que sean menores a 1/2
+        rationals.stream().filter(r->r.isLessThan(unMedio)).forEach(System.out::println);
+        //Filtra(busca menor) e imprime
+        
+        System.out.println("");
+        //Sumar todos los racionales
+        var suma = new Rational(0, 1);
+        rationals.stream().map(r->r.plus(suma)).forEach(System.out::println);
+        
+        //int sum = rationals.stream().reduce(0, (a, b) -> a + b);
+        
+        //Obtener el número racional mayor
+        
+        //Sumar todos numeradores y obtener los siguientes datos: número de elementos, la suma de los numeradores, el numerador menor, el mayor y el promedio
+        
+        
+        //int [] array = {23,43,56,97,32};
+        // Establecer el valor de inicio. El resultado será el valor de inicio + la suma de la matriz.
+        //int startValue = 0;
+        //int sum = Arrays.stream(array).reduce(startValue, (x, y) -> x + y);
+        //System.out.println (sum);
+        //sum = Arrays.stream (array) .reduce (startValue, Integer :: sum);
+        //System.out.println (sum);
+        
     }
 }
 
